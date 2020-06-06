216544b0e2828819e3e1da87d8e8cb2ac9db5544
Slawek <yarpiin@gmail.com>
2020-06-06 13:01:17 +0100

    Revert "sched/fair: Fix issue where frequency update not skipped"
    This reverts commit 6f6a13b21922abe0a6c738c6dedc95fe71fd3191

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3657,6 +3657,7 @@ update_cfs_rq_load_avg(u64 now, struct cfs_rq *cfs_rq, bool update_freq)
 
 #define UPDATE_TG	0x0
 #define SKIP_AGE_LOAD	0x0
+#define SKIP_CPUFREQ	0x3
 
 static inline void update_load_avg(struct sched_entity *se, int not_used1)
 {
@@ -5131,7 +5132,7 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		if (cfs_rq_throttled(cfs_rq))
 			break;
 
-		update_load_avg(se, UPDATE_TG);
+		update_load_avg(se, UPDATE_TG | (flags & DEQUEUE_IDLE));
 		update_cfs_shares(se);
 	}
 
